0b24bd630da9c7352a4566e8bd851042d80ed59c
DevilishDagon <152270339+DevilishDagon@users.noreply.github.com>
2025-05-23 14:21:31 +0200

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -3,3 +3,12 @@ def faculteit(n):
     while n > 1:
         totaal *= n
     return n
+
+
+if __name__ is "__main__":
+    print(faculteit(0))
+    print(faculteit(1))
+    print(faculteit(2))
+    print(faculteit(3))
+    print(faculteit(4))
+    print(faculteit(5))
