bd3e4152508a91b4a387445f0b823b8d192e28fe
devilishdagon <koktim2003@gmail.com>
2025-05-23 14:33:22 +0200

    First version

--- Python/wiskunde.py
+++ Python/wiskunde.py
@@ -2,13 +2,18 @@ def faculteit(n):
     totaal = 1
     while n > 1:
         totaal *= n
-    return n
+        n -= 1
+    return totaal
 
 
-if __name__ is "__main__":
+if __name__ == "__main__":
+    print("---START TEST---")
+
     print(faculteit(0))
     print(faculteit(1))
     print(faculteit(2))
     print(faculteit(3))
     print(faculteit(4))
     print(faculteit(5))
+
+    print("---END TEST---")
